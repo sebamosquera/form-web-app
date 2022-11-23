a8405311bd1bb6bfebb4bd3dd8728211e22c9b02
Sebastian <sebastianmosquera86@gmail.com>
2022-11-23 20:42:34 -0300

    added Mapa component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import React from 'react';
 import Formulario from './Formulario/formulario';
 import Marcador from './Marcador/marcador';
-
+import Mapa from './Mapa/mapa';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <Formulario />
         <Marcador />
+        <Mapa className="map-div"/>
       </header>
     </div>
   );
